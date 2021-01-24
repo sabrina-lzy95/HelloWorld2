75cdc52b303bd1d4ac521bc231608ff557c82465
Sabrina Lee <1902692@sit.singaporetech.edu.sg>
2021-01-24 15:53:37 +0800

    Android Lab 1 Exercise 1

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //TODO: add your logging message here.
+        Log.d("MainActivity","Attack of the killer androids")
     }
     
 }
